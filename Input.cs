1e770430b97f481fccf8fc702b1d3ec0d1321c31
Krzysztof Celinski <krzysztof.celinski@gmail.com>
2024-01-12 10:01:09 +0100

    init

---
+++ Input.cs
@@ -0,0 +1,4 @@
+public class Input
+{
+    public decimal? SomeValue { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using RulesEngine.Models;
+
+var simpleWorkflow = await File.ReadAllTextAsync("SimpleWorkflow.json");
+
+var input = new Input()
+{
+    SomeValue = null
+};
+
+Console.WriteLine($"null < 20 in dotnet: {input.SomeValue < 20}");
+
+var ruleEngine = new RulesEngine.RulesEngine(
+    new[]
+    {
+        simpleWorkflow
+    });
+
+var resultList = await ruleEngine.ExecuteAllRulesAsync("SimpleWorkflow", new RuleParameter("fact", input));
+
+Console.WriteLine($"null < 20 in RulesEngine: {resultList.First().IsSuccess}");
